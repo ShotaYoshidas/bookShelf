8779c55e36ee244db998d430fcc8889da8c136de
ShotaYoshida <soccer26sy@icloud.com>
2023-09-10 14:50:18 +0900

    コメント修正

--- BookShelf/Model/TextSearchBookModel.swift
+++ BookShelf/Model/TextSearchBookModel.swift
@@ -19,7 +19,6 @@ struct TopTier : Codable {
 
 struct Item: Codable {
     var volumeInfo: VolumeInfo?
-    
 }
 
 struct VolumeInfo: Codable {
@@ -31,27 +30,39 @@ struct ImageLinks: Codable {
     var thumbnail:String?
 }
 
-
 final class TextSearchBookModel {
-    weak var delegate: SearchBookModelDelegate? = nil //行き先未定
+    weak var searchBookDelegate: SearchBookModelDelegate? = nil //行き先未定
+    //アクセス制限はするが読み取りは可能でprivateよりは制限弱い
     private(set) var response: TopTier? = nil {
+        //プロパティ(response)の値が変更された後に呼びたい処理を記述
         didSet {
-            //バックグランドに色々処理が動く。それが終わったらDispatchQueueの処理にいく
+            //responseに値が全て格納されてからUIの更新を行いたい。このデータ取得の処理の中で一番最後に実行されてた。
+            //また、CollectionView.reloadDataの処理がmainスレッドになった。なんで？
             DispatchQueue.main.async {
-                self.delegate?.updateCollectionView()
+                if let collectonDelegate = self.searchBookDelegate{
+                    collectonDelegate.updateCollectionView()
+                }
             }
         }
     }
     
     func getBooks(encodedString: String) {
-        let GoogleBooksAPI = GoogleBooksAPI(keyword: encodedString)
+        let googleBooksAPI = GoogleBooksAPI(keyword: encodedString)
+        //アプリケーションの根源は同期環境から始まるのでasyncなメソッドを呼び出す際にどこかで必ずTaskを使う必要がある。
         Task {
-            self.response = try await GoogleBooksAPI.getBookData()
+            do {
+                //非同期処理が完了すると、その結果が response プロパティに設定される
+                self.response = try await googleBooksAPI.getBookData()
+                print(response)
+                //DispatchQueue.main.asyncでUI更新してgetBooks終了。
+            } catch {
+                print(error)
+            }
         }
     }
     
     func bookIsEmpty() {
        response = nil
-        self.delegate?.updateCollectionView()
+        self.searchBookDelegate?.updateCollectionView()
     }
 }
